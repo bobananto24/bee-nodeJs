de8dfd6126e6e4c2c95a3ca4fb2dc3d2f2252ab8
Boban Anto B <55755508+bobananto24@users.noreply.github.com>
2021-07-31 20:51:16 +0530

    structured response

--- routes/TodoList.js
+++ routes/TodoList.js
@@ -7,7 +7,7 @@ router.post("/addTodo/:userId", middleware.verify, async (req, res) => {
   const newTodo = new Todo({ name: req.body.name, userId: req.params.userId });
   try {
     const todo = await newTodo.save();
-    res.status(200).json(todo);
+    res.status(200).json({ data: todo });
   } catch (e) {
     res.status(500).json(e);
   }
@@ -16,7 +16,7 @@ router.post("/addTodo/:userId", middleware.verify, async (req, res) => {
 router.get("/listTodo/:userId", middleware.verify, async (req, res) => {
   try {
     const todo = await Todo.find({ userId: req.params.userId });
-    res.status(200).json(todo);
+    res.status(200).json({ data: todo });
   } catch (e) {
     res.status(500).json(e);
   }
@@ -35,7 +35,7 @@ router.patch("/updateTodo/:todoId", middleware.verify, async (req, res) => {
     const todo = await Todo.findById(req.params.todoId);
     todo.completed = req.body.completed;
     const savedTodo = await todo.save();
-    res.status(200).json(savedTodo);
+    res.status(200).json({ data: savedTodo });
   } catch (e) {
     res.status(500).json(e);
   }
